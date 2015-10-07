477d5911dd6a740b6e7e5a7a0539c0e14cce6918
Steven Feldman <mr.stevenfeldman@gmail.com>
2015-10-06 21:30:20 -0400

    Changed how reserved bits were safeguarded, added join to prevent incorrect behaior from calling pop_back_only and push_back_only at the same time, which should never occur, use other pop/push api instead

--- database/source/main.cc
+++ database/source/main.cc
@@ -7,13 +7,33 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
+#include <cstdint>
 
 // glibc-specific includes
 #include <execinfo.h>
 #include <signal.h>
 #include <unistd.h>
 
+// This is needed so that tervel doesn't assert.
+struct tervel_uint64_t
+{
+    uint64_t value_;
+    tervel_uint64_t(uint64_t v=0)
+        : value_(v) {}
+
+
+    uint64_t value() {
+        return value_;
+    }
+
+    uint64_t value_a() {
+        return value_ << 3;
+    }
+
+    uint64_t value_b() {
+        return value_ >> 3;
+    }
+};
 
 void handler(int signal)
 {
@@ -32,12 +52,17 @@ typedef tervel::containers::wf::vector::Vector<uint64_t> tervelVector;
 // This is the thread function
 void add_hello_world(uint64_t thread_id, tervel::Tervel* tervel, tervelVector* vector)
 {
-    if(vector == nullptr) { printf("vector object null!\n"); return; }
+    if(vector == nullptr) {
+        printf("vector object null!\n");
+        return;
+    }
 
     tervel::ThreadContext* thread_context = new tervel::ThreadContext(tervel);
 
-    size_t ret = vector->push_back_only(thread_id);
-    //printf("thread_id: %lu\n", thread_id);
+    tervel_uint64_t temp(thread_id);
+
+    size_t ret = vector->push_back_only(temp.value_a());
+    printf("idx: %u tid: %lu pushed: %lu\n",ret, thread_id, temp.value_a());
 
     delete thread_context;
 
@@ -46,17 +71,7 @@ void add_hello_world(uint64_t thread_id, tervel::Tervel* tervel, tervelVector* v
     return;
 }
 
-// This is needed so that tervel doesn't assert.
-typedef union
-{
-    uint64_t value;
 
-    struct
-    {
-        uint64_t tervel_reserved : 2;   // These are reserved by Tervel
-        uint64_t tervel_value : 62;     // The actual integer value
-    };
-} tervel_uint64_t;
 
 int main (int argc, char** argv)
 {
@@ -71,9 +86,10 @@ int main (int argc, char** argv)
 
     tervel::Tervel* tervel;
     tervelVector* test_vector;
-    
+
+    int nThreads = 10;
     // Tervel initialization code
-    tervel = new tervel::Tervel(5);
+    tervel = new tervel::Tervel(nThreads);
     test_vector = new tervelVector(64);
 
     // The main thread counts as a thread
@@ -81,27 +97,27 @@ int main (int argc, char** argv)
 
     // Initialize and start the threads
     std::vector<std::thread> threads;
-    for(int i = 1; i < 5; i++)
-    {
-        tervel_uint64_t v;
-        v.tervel_reserved = 0;
-        v.tervel_value = i;
 
-        std::thread th(add_hello_world, v.value, tervel, test_vector);
+    for(int i = 1; i < nThreads; i++)
+    {
+        std::thread th(add_hello_world, i, tervel, test_vector);
         threads.push_back(std::move(th));
     }
 
-    while(true)
+    for(auto i = threads.begin(); i != threads.end(); i++)
+    {
+        (*i).join();
+    }
+
+    for(int i = 1; i < nThreads; i++)
     {
         // Print out what was put into the vector
         uint64_t t = 0;
-        tervel_uint64_t id;
-        id.value = 0;
 
         if(test_vector->pop_back_only(t))
         {
-            id.value = t;
-            printf("Hello World %lu\n", id.tervel_value);
+            tervel_uint64_t temp(t);
+            printf("PoPed: %lu %lu\n", t, temp.value_b());
         }
     }
 
