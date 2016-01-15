dc5013de596e6e0fad4ae0837a313697fae9b205
Micharl McGee <mcgee2515@gmail.com>
2016-01-14 17:51:37 -0800

    Added CommandType enum and NetworkPacket struct Feel free to change how I did the constants, idk what is best practice

---
+++ database/include/util/libomdb.h
@@ -0,0 +1,28 @@
+//
+// Created by mike on 1/14/16.
+//
+
+#ifndef OPENMEMDB_LIBOMDB_H
+#define OPENMEMDB_LIBOMDB_H
+
+#include <string>
+
+#define MESSAGE_SIZE 300
+
+const int THE_TERMINATOR = 0xFF;
+
+/**
+ * Enum used to describe the type of packet being sent
+ */
+enum CommandType {
+  SQL_STATEMENT = 0x01,
+  DB_COMMAND = 0x02
+};
+
+typedef struct NetworkPacket {
+  CommandType commandType;
+  char message[MESSAGE_SIZE];
+  uint8_t terminator;
+} NetworkPacket;
+
+#endif //OPENMEMDB_LIBOMDB_H
