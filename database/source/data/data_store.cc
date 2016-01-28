898ed645565a1cd489b815d2e7be099b70ed0057
heyblackduck <r.median93@gmail.com>
2016-01-28 14:41:48 -0500

    comments added to deleteTable

--- database/source/data/data_store.cc
+++ database/source/data/data_store.cc
@@ -7,6 +7,16 @@
 // Project includes
 #include "data/data_store.h"
 
+//TODO: Test schema checker for more constraints
+//Checks if the column type matches the data type and constraint of the schema for that column
+//Should return error codes depending on the conflict (probably ManipResult)
+//think about this: parsing contraints, error codes, type of contraints, efficency of SQLColumn, memory
+void DataStore::schemaChecker(TableSchema schema, std::vector<SQLColumn> column)
+{
+	//check data type
+	//check constraint
+}
+
 // TODO: Doxygenify this documentation
 //creates an empty table
 ManipResult DataStore::createTable(CreateTableCommand table_info)
@@ -46,9 +56,27 @@ ManipResult DataStore::createTable(CreateTableCommand table_info)
 
 /**
  * TODO
+ * Must find table in table mapping
+ * Find table and delete all rows
+ * Must delete schema pair and then delete table from table_mapping
  */
 ManipResult DataStore::deleteTable(std::string table_name)
 {
+    //find table in table_mapping
+    SchemaTablePair* table_pair = getTablePair(table_name);
+    if(table_pair == nullptr)
+    {
+        return ManipResult(ResultStatus::ERROR_INVALID_TABLE, ManipStatus::FAILURE);
+    }
+
+    DataTable *table = &table_pair.table;
+    TableSchema *schema = &table_pair.schema;
+    //delete table from pair
+    //delete schema from pair
+    //delete pair (schema and table)
+    //delete table from table_mapping
+
+    /*
     // Remove the table from the table name map
     // TODO: Perform proper memory clean-up after removal from mapping
     // TODO: What does access_counter != 0 mean?
@@ -59,6 +87,7 @@ ManipResult DataStore::deleteTable(std::string table_name)
     }
 
     return ManipResult(ResultStatus::SUCCESS, ManipStatus::SUCCESS);
+    */
 }
 
 /**
