87077806dbc1792bf9fa811d5e390c4fe21391d6
JStav <hiorelse@gmail.com>
2016-02-28 18:31:38 -0500

    Initial automatic plot creation
    Add python script to create a plot
    Add bash script to automate test execution and plot creation
    Created GraphTest class for test graphing, for now it is called in main but eventually it will
    be its own executable
    
    Create Test only for now

--- database/tests/main.cc
+++ database/tests/main.cc
@@ -3,28 +3,21 @@
 #include "DataStoreTest.h"
 #include "Modes.h"
 #include "TestResult.h"
-
+#include "GraphTest.h"
 
 int main()
 {
-    
-    // Test test;
-    
-    // int testMode = MODE_CREATE;"CREATE TABLE TestTable (A INTEGER, B INTEGER);"
-    
-    // test.with(testMode)
-    //     .generateCases(1, 100)
-    //     .test();
-    
+    // DataStoreTest dataStoreTest;
 
-    DataStoreTest dataStoreTest;
+    // TestResult result = dataStoreTest.with(MODE_CREATE)
+    // 				                 .generateCases(0b1000)
+    // 				                 .test();
 
-    TestResult result = dataStoreTest.with(MODE_CREATE)
-    				                 .generateCases(0b1000)
-    				                 .test();
+    // printf("Duration: %lu\n", result.duration);
+    // printf("Thread Count: %d\n", result.threadCount);
 
-    printf("Duration: %lu\n", result.duration);
-    printf("Thread Count: %d\n", result.threadCount);
+
+    GraphTest graphTest;
 
 	return 1;
 }
