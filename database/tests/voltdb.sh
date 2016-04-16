1fd065b464e0579fe2e5ad904760304a85cc4903
Robert <heyblackduck@yahoo.com>
2016-04-16 17:09:27 -0400

    need to merge on server

--- database/tests/voltdb.sh
+++ database/tests/voltdb.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-
 filename="sql_statements.omdbt"
 declare -a create
 declare -a insert
 declare -a select
 declare -a drop
 flag=""
+test=""
 
 #Reads file and determines how each line will be stored based on flags
+test=$(head -n 1 $filename)
+echo $test
 while read line
 do
     #Set up flag to determine how the read line will be used
@@ -16,14 +17,34 @@ do
         "insert" ) flag="insert" ;;
         "select" ) flag="select" ;;
         "drop" ) flag="drop" ;;
+        *) continue
     esac
 
-    if [ "$flag" == "create" ]; then
+    #Copies data into array depending on flag and makes sure that flag is not copied as well
+    if [ "$flag" == "create" ] && [ "$line" != "$flag" ]; then
         create+=("$line")
     fi
 
-    #do this for every other flag
-    
+    if [ "$flag" == "insert" ] && [ "$line" != "$flag" ]; then
+        insert+=("$line")
+    fi
+
+    if [ "$flag" == "select" ] && [ "$line" != "$flag" ]; then
+        select+=("$line")
+    fi
+
+    if [ "$flag" == "drop" ] && [ "$line" != "$flag" ]; then
+        drop+=("$line")
+    fi
+
 done < $filename
 
-echo $flag \ ${create[@]}
+#print array
+printf "%s\n" "${create[@]}" "${insert[@]}" "${select[@]}" "${drop[@]}" >> output.sql
+
+#TODO: start voltdb
+
+if [ $test == "create_test" ]; then
+    startTime=$(date +%s%N)/1000 
+    while 
+
