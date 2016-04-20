7f40a5f0bacb816a6f664603c7fa102416e18955
Mike McGee <mcgee2515@gmail.com>
2016-04-19 22:03:11 -0700

    Updated memsql tester to handle mix test

--- database/tests/db_testing/memsql_test/src/Commands.java
+++ database/tests/db_testing/memsql_test/src/Commands.java
@@ -9,11 +9,15 @@ public class Commands {
     private final ArrayList<String> createCommands;
     private final ArrayList<String> insertCommands;
     private final ArrayList<String> selectCommands;
+    private final ArrayList<String> starterInserts;
+    private final ArrayList<String> mixedCommands;
 
     public Commands(List<String> commands) {
         this.createCommands = getCommands(commands, "CREATE");
         this.insertCommands = getCommands(commands, "INSERT");
         this.selectCommands = getCommands(commands, "SELECT");
+        this.starterInserts = getBeginingInserts(commands);
+        this.mixedCommands = getMixedCommands(commands);
     }
 
     public ArrayList<String> getCreateCommands() {
@@ -28,7 +32,49 @@ public class Commands {
         return selectCommands;
     }
 
+    public ArrayList<String> getStarterInserts(){return starterInserts;}
+
+    public ArrayList<String> getMixedCommands() {
+        return mixedCommands;
+    }
+
     private ArrayList<String> getCommands(List<String> commands, String start) {
         return commands.stream().filter(s -> s.startsWith(start)).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private ArrayList<String> getBeginingInserts(List<String> commands) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // Progress up to insert statements
+        int i = 0;
+        while (!commands.get(i).equalsIgnoreCase("insert") && i != commands.size()) {
+            i++;
+        }
+
+        // Add all insert statements until you get to mix
+        while (!commands.get(i).equalsIgnoreCase("mixed") && i != commands.size()) {
+            // Test
+            if (commands.get(i).startsWith("INSERT")) {
+                toReturn.add(commands.get(i));
+            }
+            ++i;
+        }
+
+        return toReturn;
+    }
+
+    private ArrayList<String> getMixedCommands(List<String> commands) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // Move up to mixed statement
+        int i = 0;
+        while (!commands.get(i).equalsIgnoreCase("mixed") && i != commands.size()) {
+            ++i;
+        }
+
+        // Add all remaining commands to toReturn
+        while (i < commands.size()) {
+            toReturn.add(commands.get(i));
+        }
+
+        return toReturn;
     }
 }
